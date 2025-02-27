a0369ea138a885c143cf3952587eb8a2bde627ec
Luis <luis.subiabre@gmail.com>
2025-02-27 10:03:18 -0300

    Feat: Error Boundary para capturar errores

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { AuthProvider } from "@/context/AuthProvider";
 import ProtectedRoute from "@/components/ProtectedRoute"; // Asegúrate de que ProtectedRoute está correctamente importado
 import Spinner from "./components/Spinner";
+import ErrorBoundary from "@/components/ErrorBoundary"; // Importa el ErrorBoundary
 
 const Login = lazy(() => import("@/app/login/page"));
 const Logout = lazy(() => import("@/app/logout/pageLogout"));
@@ -46,174 +47,176 @@ function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <AuthProvider>
-        <Suspense
-          fallback={
-            <>
-              <div className="flex justify-center items-center h-full w-2/5 mx-auto">
-                <Spinner />
-              </div>
-              <div className="flex justify-center items-center h-full w-2/5 mx-auto">
-                Un momento por favor...
-              </div>
-            </>
-          }
-        >
-          <Routes>
-            {/* Public Routes */}
+        <ErrorBoundary>
+          <Suspense
+            fallback={
+              <>
+                <div className="flex justify-center items-center h-full w-2/5 mx-auto">
+                  <Spinner />
+                </div>
+                <div className="flex justify-center items-center h-full w-2/5 mx-auto">
+                  Un momento por favor...
+                </div>
+              </>
+            }
+          >
+            <Routes>
+              {/* Public Routes */}
 
-            <Route path="/login" element={<Login />} />
-            <Route path="/logout" element={<Logout />} />
-            <Route path="/unauthorized" element={<UnauthorizedComponent />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/logout" element={<Logout />} />
+              <Route path="/unauthorized" element={<UnauthorizedComponent />} />
 
-            {/* Protected Routes */}
-            <Route
-              path="/"
-              element={
-                <ProtectedRoute>
-                  <Layout>
-                    <Dashboard />
-                  </Layout>
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/dashboard"
-              element={
-                <ProtectedRoute>
-                  <Layout>
-                    <Dashboard />
-                  </Layout>
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/dashboard/toor/usuarios"
-              element={
-                <ProtectedRoute requiredRoles={[1]}>
-                  <Layout>
-                    <Usuarios />
-                  </Layout>
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/dashboard/toor/roles"
-              element={
-                <ProtectedRoute>
-                  <Layout>
-                    <Roles />
-                  </Layout>
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/dashboard/toor/cursos"
-              element={
-                <ProtectedRoute requiredRoles={[1]}>
-                  <Layout>
-                    <Cursos />
-                  </Layout>
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/dashboard/toor/estudiantes"
-              element={
-                <ProtectedRoute>
-                  <Layout>
-                    <Estudiantes />
-                  </Layout>
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/dashboard/toor/asignaturas"
-              element={
-                <ProtectedRoute requiredRoles={[1]}>
-                  <Layout>
-                    <Asignaturas />
-                  </Layout>
-                </ProtectedRoute>
-              }
-            />
+              {/* Protected Routes */}
+              <Route
+                path="/"
+                element={
+                  <ProtectedRoute>
+                    <Layout>
+                      <Dashboard />
+                    </Layout>
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/dashboard"
+                element={
+                  <ProtectedRoute>
+                    <Layout>
+                      <Dashboard />
+                    </Layout>
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/dashboard/toor/usuarios"
+                element={
+                  <ProtectedRoute requiredRoles={[1]}>
+                    <Layout>
+                      <Usuarios />
+                    </Layout>
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/dashboard/toor/roles"
+                element={
+                  <ProtectedRoute>
+                    <Layout>
+                      <Roles />
+                    </Layout>
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/dashboard/toor/cursos"
+                element={
+                  <ProtectedRoute requiredRoles={[1]}>
+                    <Layout>
+                      <Cursos />
+                    </Layout>
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/dashboard/toor/estudiantes"
+                element={
+                  <ProtectedRoute>
+                    <Layout>
+                      <Estudiantes />
+                    </Layout>
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/dashboard/toor/asignaturas"
+                element={
+                  <ProtectedRoute requiredRoles={[1]}>
+                    <Layout>
+                      <Asignaturas />
+                    </Layout>
+                  </ProtectedRoute>
+                }
+              />
 
-            <Route
-              path="/dashboard/academico/inicio"
-              element={
-                <ProtectedRoute requiredRoles={[2]}>
-                  <Layout>
-                    <AcademicoInicio />
-                  </Layout>
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/dashboard/academico/calificaciones/:id"
-              element={
-                <ProtectedRoute requiredRoles={[2]}>
-                  <Layout>
-                    <Calificaciones />
-                  </Layout>
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/dashboard/inspectoria/asistencia"
-              element={
-                <ProtectedRoute requiredRoles={[3]}>
-                  <Layout>
-                    <InspectoriaAsistencia />
-                  </Layout>
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/dashboard/inspectoria/atrasos"
-              element={
-                <ProtectedRoute requiredRoles={[3]}>
-                  <Layout>
-                    <h1>Inspectoria Atrasos</h1>
-                  </Layout>
-                </ProtectedRoute>
-              }
-            />
+              <Route
+                path="/dashboard/academico/inicio"
+                element={
+                  <ProtectedRoute requiredRoles={[2]}>
+                    <Layout>
+                      <AcademicoInicio />
+                    </Layout>
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/dashboard/academico/calificaciones/:id"
+                element={
+                  <ProtectedRoute requiredRoles={[2]}>
+                    <Layout>
+                      <Calificaciones />
+                    </Layout>
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/dashboard/inspectoria/asistencia"
+                element={
+                  <ProtectedRoute requiredRoles={[3]}>
+                    <Layout>
+                      <InspectoriaAsistencia />
+                    </Layout>
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/dashboard/inspectoria/atrasos"
+                element={
+                  <ProtectedRoute requiredRoles={[3]}>
+                    <Layout>
+                      <h1>Inspectoria Atrasos</h1>
+                    </Layout>
+                  </ProtectedRoute>
+                }
+              />
 
-            <Route
-              path="/dashboard/acles/talleres"
-              element={
-                <ProtectedRoute requiredRoles={[1]}>
-                  <Layout>
-                    <AcleTalleres />
-                  </Layout>
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/dashboard/acles/talleres/inscritos/:id"
-              element={
-                <ProtectedRoute requiredRoles={[1]}>
-                  <Layout>
-                    <AclesInscritos />
-                  </Layout>
-                </ProtectedRoute>
-              }
-            />
+              <Route
+                path="/dashboard/acles/talleres"
+                element={
+                  <ProtectedRoute requiredRoles={[1]}>
+                    <Layout>
+                      <AcleTalleres />
+                    </Layout>
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/dashboard/acles/talleres/inscritos/:id"
+                element={
+                  <ProtectedRoute requiredRoles={[1]}>
+                    <Layout>
+                      <AclesInscritos />
+                    </Layout>
+                  </ProtectedRoute>
+                }
+              />
 
-            <Route
-              path="/dashboard/jefatura"
-              element={
-                <ProtectedRoute>
-                  <Layout>
-                    <Jefatura />
-                  </Layout>
-                </ProtectedRoute>
-              }
-            />
+              <Route
+                path="/dashboard/jefatura"
+                element={
+                  <ProtectedRoute>
+                    <Layout>
+                      <Jefatura />
+                    </Layout>
+                  </ProtectedRoute>
+                }
+              />
 
-            {/* Redirect to login if not authenticated */}
-            <Route path="*" element={<Navigate to="/login" />} />
-          </Routes>
-        </Suspense>
+              {/* Redirect to login if not authenticated */}
+              <Route path="*" element={<Navigate to="/login" />} />
+            </Routes>
+          </Suspense>
+        </ErrorBoundary>
       </AuthProvider>
     </ThemeProvider>
   );
