cc7e53cc9911f11a558da84065d306998187d93a
Luis <luis.subiabre@gmail.com>
2025-02-26 23:00:42 -0300

    refactor: asignaturas

--- src/types/index.ts
+++ src/types/index.ts
@@ -50,3 +50,58 @@ export interface FuncionarioType {
   fecha_actualizacion: string;
   clave?: string; // Agregamos password como opcional
 }
+
+/* Asignaturas */
+export interface Link {
+  rel: string;
+  href: string;
+  method: string;
+}
+
+export interface AsignaturaType {
+  asignatura_id: number;
+  nombre: string;
+  descripcion: string;
+  indice: number;
+  concepto: boolean;
+  codigo_sige: number;
+  nivel_educativo: number;
+  es_comun: boolean;
+}
+
+export interface AsignaturaCursoUsuarioType {
+  asignatura_id: number;
+  curso_id: number;
+  profesor_id: number;
+}
+
+export interface AsignacionPendienteType {
+  curso_id: number;
+  usuarios: number[];
+}
+
+export interface AsignaturaCursoResponseType {
+  asignatura_id: number;
+  curso_id: number;
+  profesor_id: number;
+  asignatura_nombre: string;
+  curso_nombre: string;
+  curso_descripcion: string;
+  profesor_nombre: string;
+  profesor_email: string;
+}
+
+export interface ApiResponse<T> {
+  data: T[];
+  links: Link[];
+}
+
+export interface AsignaturasResponseType {
+  info: {
+    count: number;
+    pages: number;
+    next: string | null;
+    prev: string | null;
+  };
+  asignaturas: AsignaturaType[];
+}
